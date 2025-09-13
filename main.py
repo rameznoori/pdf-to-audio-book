115059c3ba8755d8c8ffa6a860c932e1fb3ad801
rameznoori <199920071+rameznoori@users.noreply.github.com>
2025-09-14 00:18:35 +0530

    added a for loop to loop through text in a pdf decument

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ import PyPDF2
 from tkinter.filedialog import *
 
 book = askopenfilename()
-pdf_reader = PyPDF2.PdfFileReader(book)
-pages = pdf_reader.numPages
+pdf_reader = PyPDF2.PdfReader(book)
+pages = len(pdf_reader.pages)
 
+player = pyttsx3.init()
 
+for num in range(pages):
+    page = pdf_reader.pages[num]
+    text = page.extract_text()
+    player.say(text)
+    player.runAndWait()
